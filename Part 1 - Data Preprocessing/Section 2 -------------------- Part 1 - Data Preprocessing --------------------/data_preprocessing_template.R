f91212916eaaec961c8bd4e92629f43c29b933f2
RustamJadara <RustamJadara@gmail.com>
2018-06-10 17:18:20 +0530

    added linear regression

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -0,0 +1,7 @@
+#Data Preprocessing
+
+#Import the dataset
+dataset = read.csv("Data.csv")
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age,FUN = function(x) mean(x,na.rm=TRUE)),
+                     dataset$Age)
